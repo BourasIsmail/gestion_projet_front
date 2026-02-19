c0688498989854e9fb2e2edcc4c1a452caad8bab
ismail <ismailbouras1968@gmail.com>
2026-02-19 13:42:49 +0100

    api

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import { cookies } from "./cookies"
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api"
+const API_BASE = process.env.NEXT_PUBLIC_API_URL || /*"http://localhost:8080/api"*/"http://172.16.10.60:8080/api"
 
 async function request<T>(path: string, options: RequestInit = {}): Promise<T> {
     const token = typeof window !== "undefined" ? cookies.getToken() : null
